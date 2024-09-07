0f304dd8f136e03ed1f64389b0d6e448ce6cb084
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2024-09-07 15:14:03 +0800

    added same user.email validation

--- server-actions/auth.js
+++ server-actions/auth.js
@@ -2,6 +2,7 @@
 
 import { hashUserPassword } from "@/lib/hash";
 import { createUser } from "@/lib/user";
+import { redirect } from "next/navigation";
 
 export async function signup(prevState, formData) {
   const email = formData.get("email");
@@ -21,6 +22,21 @@ export async function signup(prevState, formData) {
     return {
       errors,
     };
+
   const hashedPassword = hashUserPassword(password);
-  createUser(email, hashedPassword);
+
+  try {
+    createUser(email, hashedPassword);
+  } catch (error) {
+    if (error.code === "SQLITE_CONSTRAINT_UNIQUE") {
+      return {
+        errors: {
+          email: "The email is already existed.",
+        },
+      };
+    }
+    throw error;
+  }
+
+  redirect("/training");
 }
