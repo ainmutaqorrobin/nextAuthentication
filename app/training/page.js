55dc902d80b73f600f8a42b0edfad6a624380528
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2024-09-08 16:10:45 +0800

    added protected routes

--- app/training/page.js
+++ app/training/page.js
@@ -1,6 +1,13 @@
-import { getTrainings } from '@/lib/training';
+import { verifyAuth } from "@/lib/auth";
+import { getTrainings } from "@/lib/training";
+import { redirect } from "next/navigation";
 
 export default async function TrainingPage() {
+  const result = await verifyAuth();
+  console.log(result);
+
+  if (!result.user) return redirect("/");
+
   const trainingSessions = getTrainings();
 
   return (
